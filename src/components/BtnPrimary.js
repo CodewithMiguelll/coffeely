34341332348689bc841f6eb1e0dd805ea851b34e
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2024-07-01 10:59:35 +0100

    feat: added sliding animation to the mobile menu, set up the motion component for the primary button.

--- src/components/BtnPrimary.js
+++ src/components/BtnPrimary.js
@@ -1,7 +1,15 @@
+import {motion} from 'framer-motion';
+
 const BtnPrimary = ({label}) => {
     return ( 
-        <button 
-         className="border p-2 rounded-md bg-[#6d4c41] text-[#fafafa] transition-all active:bg-[#4d3128] hover:translate-y-1 active:translate-y-2">{label}</button>
+        <motion.button 
+        initial={{
+
+        }}
+        animate={{
+            
+        }}
+         className="border p-2 rounded-md bg-[#6d4c41] text-[#fafafa] transition-all active:bg-[#4d3128] hover:translate-y-1 active:translate-y-2">{label}</motion.button>
      );
 }
  
