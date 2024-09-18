d23f287cc454e9a834568d08c33688cb7499e8b6
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2024-09-18 18:02:28 +0100

    feat: removed unneccasary hover states from the primary and secondary button components.

--- src/components/BtnPrimary.js
+++ src/components/BtnPrimary.js
@@ -1,8 +1,8 @@
 const BtnPrimary = ({label}) => {
     return (
-        <button className="border p-2 rounded-md bg-[#6d4c41] text-[#fafafa] transition-all active:bg-[#4d3128] hover:translate-y-1 active:translate-y-2">
-          {label}
-        </button>
+      <button className="border p-2 rounded-md bg-[#6d4c41] text-[#fafafa] transition-all hover:bg-[#63463d] active:bg-[#4d3128]">
+        {label}
+      </button>
     );
 }
  
--- src/components/BtnSecondary.js
+++ src/components/BtnSecondary.js
@@ -1,7 +1,9 @@
 const BtnSecondary = ({label}) => {
-    return ( 
-        <button className="border p-2 rounded-md bg-[#a1887f] text-[#fafafa] transition-all active:bg-[#7e655c] hover:translate-y-1 active:translate-y-2">{label}</button>
-     );
+    return (
+      <button className="border p-2 rounded-md bg-[#a1887f] text-[#fafafa] transition-all active:bg-[#7e655c] hover:bg-[#7e6f69]">
+        {label}
+      </button>
+    );
 }
  
 export default BtnSecondary;
